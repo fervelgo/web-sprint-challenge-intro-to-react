e17a10229283814861cda15b3ed15594aa49b5dc
Fernando Velasco <fervelgo@gmail.com>
2021-05-14 15:40:28 -0500

    fixed the URL for the get request

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Character from './components/Character';
 import './App.css';
+import { setupWorker, rest } from 'msw';
 
 const App = () => {
  
@@ -18,15 +19,16 @@ const App = () => {
 
 useEffect(()=> {
   axios
-  .get(`https://swapi.dev/api/films`)
+  .get(`https://swapi.dev/api/people`)
   .then((res) => {
-    setCharacters(res.results)
-    console.log(res.results)
+    setCharacters(res.data)
+    console.log(res.data)
   })
   .catch(err => {
     console.log(err)  
   })
 },[])
+
    // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
@@ -38,8 +40,8 @@ useEffect(()=> {
     <div className="App">
       <h1 className="Header">Characters</h1>
         {
-          characters.map(()=>{
-            return (Character key={} action={openInfo})
+          characters.map((nm)=>{
+            return <Character key={nm.name} info={nm} action={openInfo}/>
           })
         }
     </div>
