d259786156f90527894aff79699860cd84087b40
Fernando Velasco <fervelgo@gmail.com>
2021-05-14 11:42:48 -0500

    scaffolding done and started to define state, Character component structure done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Character from './components/Character';
 import './App.css';
 
 const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
+ 
+ const [characters, setCharacters] = useState([])
+ const [currentCharacterName, setCurrentCharacterName] = useState(null) 
+
+ const openInfo = name => {
+   setCurrentCharacterName(name)
+ }
+
+ const closeInfo = name => {
+   setCurrentCharacterName(null)
+ }
+
+useEffect(()=> {
+  axios
+  .get(`https://swapi.dev/api/films`)
+  .then((res) => {
+    setCharacters(res.results)
+    console.log(res.results)
+  })
+  .catch(err => {
+    console.log(err)  
+  })
+},[])
+   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
@@ -12,6 +37,11 @@ const App = () => {
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+        {
+          characters.map(()=>{
+            return (Character key={} action={openInfo})
+          })
+        }
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,20 @@
-// Write your Character component here
+import React from 'react';
+import styled from 'styled-components';
+
+//const StyledComp = styled.div `
+
+//`
+
+//const StyledButton = styled.button ``
+
+const Character = ({info, action}) => {
+    return (
+        <div>
+            {info.name}
+            <button onClick={()=> action(info.name)}></button>
+        </div>
+    )
+}
+
+
+export default Character;
